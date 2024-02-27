fa55dc1dde797ab7d771929fff14f76bca3108a9
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-27 14:39:29 -0500

    remove unused import

--- controllers/passports.ts
+++ controllers/passports.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Router } from "express";
-import { fetchEvents } from "../utils/fetchEvents";
 import { fetchPassports } from "../utils/fetchPassports";
 
 class PassportsController {
